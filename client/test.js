06cf33af53879de1cee73213a2a107416dc96b94
MohammedMansuri384 <mohammed.mansuri@torontomu.ca>
2025-05-15 22:59:44 -0400

    removing test files

--- client/test.js
+++
@@ -1,4 +0,0 @@
-//This is a test file for the client side of the application
-//It contains a simple test case to check if the client is working correctly
-apparently
-//It uses the Mocha testing framework and the Chai assertion library
